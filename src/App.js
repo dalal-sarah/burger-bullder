46437feb5c76902f8c82ce8c22fae14f0fd15ca3
dalal <sarah.dalal17@gmail.com>
2020-07-28 09:25:27 +0300

    10

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Layout from './components/Layout/Layout'
+import BurgerBuilder from './containers/burgerBuillder'
 
 function App() {
   return (
     <div className="App">
-      
+      <Layout >
+        <BurgerBuilder />
+
+      </Layout>
     </div>
   );
 }
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Auxx from '../../hoc/Auxx'
+//import classes from '*.module.css';
+import classes from '../Layout/Layout.module.css'
+
+const layout = (props ) => (
+    <Auxx>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main className={classes.Content}>
+        {props.children}
+    </main>
+    </Auxx>
+);
+export default layout
